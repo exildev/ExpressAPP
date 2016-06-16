313a7763ca949c025476fb0b79a1e4b0e7fb5f3f
Yamid Pico <ypicoleal@gmail.com>
2016-06-16 17:00:19 -0500

    soporte aceptacion empleado

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -17,7 +17,7 @@ angular.module('starter.controllers', [])
   $scope.socket_pass = '123456';
   $scope.socket_user = 'user1';
   
-  $scope.socket = io('http://192.168.1.52:4000');
+  $scope.socket = io('http://192.168.0.103:4000');
   $scope.socket.emit('i-am', 'CELL');
 
   document.addEventListener('deviceready', function() {
@@ -264,5 +264,9 @@ angular.module('starter.controllers', [])
     }, function(error){
       console.log(errors)
     });
-  })
+  });
+
+  $scope.socket.on('asignar-pedido', function(pedido) {
+    alert('se asigno un pedido nuevo');
+  });
 });
